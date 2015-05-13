3a9cdbf013f2712bd506c427febca2382569aabb
Ronak <ronakp991@gmail.com>
2015-05-13 15:23:25 -0400

    Project Created

---
+++ ArduiOArt/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace GujAThon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(158, 24);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(490, 157);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "      Select File";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.Font = new System.Drawing.Font("Tw Cen MT Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
+            this.richTextBox1.Location = new System.Drawing.Point(82, 208);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(643, 179);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(807, 416);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ArduiOArt @ Guj-A-Thon ";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+
+    }
+}
+
---
+++ ArduiOArt/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+
+namespace GujAThon
+{
+    public partial class Form1 : Form
+    {
+
+        private static Color[,] pixelColor = new Color[500, 500];
+        private static string txtFileContent = "";
+        private static byte[,] blueValue = new byte[500, 500];
+        public Form1()
+        {
+            InitializeComponent();
+            richTextBox1.AppendText("Program Initialization....\n");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                Bitmap myBitmap = new System.Drawing.Bitmap(openFileDialog1.FileName);
+                richTextBox1.AppendText(openFileDialog1.FileName + "File Selected....\n");
+                var img = Image.FromFile(openFileDialog1.FileName);
+                int width = img.Width;
+                int height = img.Height;
+                richTextBox1.AppendText("Conversion of .bmp to .txt is in Progress....\n");
+                for (int i = 0; i < width; i++)
+                {
+                    for (int j = 0; j < height; j++)
+                    {
+                        pixelColor[i, j] = myBitmap.GetPixel(i, j);
+                    }
+                }
+                richTextBox1.AppendText("Conversion Complete....\n");
+                for (int i = 0; i < width; i++)
+                {
+                    for (int j = 0; j < height; j++)
+                    {
+                        blueValue[i, j] = pixelColor[i, j].B;
+                        if (blueValue[i, j] == 255) blueValue[i, j] = 0;
+                        else blueValue[i, j] = 1;
+                        txtFileContent = txtFileContent + blueValue[i, j].ToString();
+                    }
+                }
+
+                //System.IO.StreamWriter file = new System.IO.StreamWriter("c:\\Testing Debug.txt");
+                //file.WriteLine(txtFileContent);
+                richTextBox1.AppendText("");
+                //file.Close();
+                richTextBox1.AppendText(".txt File Generated....\n");
+                openFileDialog1.Dispose();
+                richTextBox1.AppendText("Communication Commencing....\n");
+                SerialPort port = new SerialPort("COM3", 9600);
+                port.Open();
+                richTextBox1.AppendText("Communication Start....\n");
+                char[] inbuf = new char[50];
+                port.Write(blueValue[0, 0].ToString());
+                for (int i = 0; i < width; i++)
+                {
+                    for (int j = 1; j < height; j++)
+                    {
+                        while (port.Read(inbuf, 0, 1) != 1) { richTextBox1.AppendText("Waiting For Response....\n"); };
+                        port.Write(blueValue[i, j].ToString());
+                    }
+                    richTextBox1.AppendText(i + "Line Completed....\n");
+                }
+                port.Close();
+                richTextBox1.AppendText("Close the Window OR Select File Again....\n");
+            }
+
+
+        }
+
+
+    }
+}
+
